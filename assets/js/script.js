7540e367bdbaa2b9569b5c897f7cfda30fa77f65
Cesar <cesare_ar@hotmail.com>
2021-07-01 17:24:00 -0500

    Json de Guantes y zapatillas

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,18 @@ let montana= document.querySelector("#montana");
 montana.addEventListener("click", ()=>{
     window.open("../montana.html", "_blank");
 });
+
+/* const fs = require('fs') */
+
+/* fs.readFile(filePath, function (error, content) {
+    var data = JSON.parse(content);
+    console.log(data.collection.length);
+});
+
+fs.readFile("/assets/texto/formato.txt"); */
+/* 
+$.getJSON("assets/texto/formato.json", function(json) {
+    console.log(json); // this will show the info it in firebug console
+    console.log("Hola");
+}); */
+
---
+++ assets/json/guantes.js
@@ -0,0 +1,174 @@
+Guantes = [
+  {
+    "nombre": "INBIKE Guantes de ciclismo de invierno",
+    "descripcion": "3 mm con almohadilla de gel térmica, pantalla táctil, resistente al viento, guantes reflectantes para deportes",
+    "marca": "INBIKE",
+    "precio": 529.99,
+    "url": "assets/images/Guantes/g001_001.PNG",
+    "talla": "",
+    "especificaciones": [
+      {
+        "Cálidos y cómodos": "los guantes están hechos de algodón cálido, forro polar y tela compuesta en el exterior y la membrana interior de TPU impermeable para aislar eficazmente el viento frío y bloquear la temperatura interior. Los guantes térmicos son ideales para hombres y mujeres en invierno, adecuados para las actividades como ciclismo de invierno, correr, motociclismo, senderismo y desplazamiento.",
+        "Almohadilla de gel de 5 mm": "la palma está equipada con una almohadilla de gel de 5 mm con la función antideslizante y antigolpes, protegiendo tus manos del impacto externo y el neumático durante el largo tiempo de conducción. Puede hacer que disfrutes de tu viaje.",
+        "Diseño reflectante": "estos guantes de bicicleta están diseñados con rayas reflectantes que se vuelven coloridos por la noche, más frescos que los guantes comunes y también juegan un papel de precaución con la alta visibilidad, lo que lo hace más seguro por la noche.",
+        "Puños elásticos alargados": "los guantes de ciclismo están alargados por el puño largo, lo que hace que sea más cálido en comparación con los guantes de como. El puño elástico puede bloquear la calidez y evitar que el aire frío del exterior. También puedes ajustar el gancho y bucle para adaptarse a tu tamaño.",
+        "visualización táctil": "las cinco puntas de los dedos están hechas de piel sintética conductiva, lo que hace que sea más conveniente usar teléfonos inteligentes sin quitar los guantes de bicicleta para responder llamadas o responder a mensajes, lo que es realmente una cosa indeseable en invierno."
+      }
+    ]
+
+  },
+
+  {
+    "nombre": "Guantes de ciclismo de dedos completos para hombres",
+    "descripcion": "tela transpirable resistente al desgaste, almohadilla de gel absorbente de golpes, apto para montar en bicicleta de montaña en otoño e invierno, Color Azul y L",
+    "marca": "IDLS",
+    "precio": 299.99,
+    "url": "assets/images/Guantes/g002_001.PNG",
+    "talla": "",
+    "especificaciones": [
+      {
+        "Descripcion": "Tejido Transpirable y Ajuste Cómodo: Hecho de LYCRA de alta elasticidad y tejido de malla de punto, con gamuza antideslizante en la parte de la palma, puede hacer que sus manos estén completamente relajadas y cómodas en el deporte de la bicicleta.",
+        "Mejorada Choque-Absorber SBR Pad": "Estos guantes de ciclismo están equipados con tres acolchados ergonómicos SBR de 5 mm en la parte de la palma. Te ayuda a mejorar el agarre, aliviar el dolor y la fatiga durante el ciclismo.",
+        "Paño de microfibra SUAVE RESISTENTE AL USO": "PAÑO DE TERRY muy absorbente en la parte exterior del pulgar, que puede limpiar fácilmente el sudor. La malla que absorbe la humedad en el centro de la palma mantiene sus manos frescas y secas.",
+        "Pantalla Táctil": "el pulgar y el índice adoptan microfibra para que puedas acceder a teléfonos inteligentes sin quitarte los guantes de carreras para responder llamadas o responder a mensajes, lo que es realmente algo indeseable en invierno.",
+        "Diseño Multiusos": "Perfecto tanto para hombres como para mujeres, lavable en la lavadora. ideal para ciclismo, senderismo, escalada, camping, etc."
+      }
+    ]
+  },
+
+  {
+    "nombre": "Guantes ESD",
+    "descripcion": "Recubrimiento en Punta de Dedo, Medianos",
+    "marca": "ESD",
+    "precio": 200.00,
+    "url": "assets/images/Guantes/g003_001.PNG",
+    "talla": "",
+    "especificaciones": [
+      {
+        "Descripcion": "Proteja electrónicos sensibles durante ensamble e inspección.Nylon sin costura y tejido de carbono conductivo.",
+        "Protección antiestática": "hasta de 108 ohmios.Reutilizable. Lavar en lavadora."
+      }
+    ]
+
+  },
+
+  {
+    "nombre": "NICEWIN Guantes de ciclismo para motocicleta",
+    "descripcion": "bicicleta de montaña, medio dedo, para hombre o mujer",
+    "marca": "NICEWIN",
+    "precio": 391.12,
+    "url": "assets/images/Guantes/g004_001.PNG",
+    "talla": "",
+    "especificaciones": [
+      {
+        "Diseño elegante": "los guantes de ciclismo NICEWIN están hechos de tela de malla elástica transpirable granular y cuero de microfibra, que son cómodos y resistentes al desgaste. El diseño de los guantes de bicicleta de medio dedo asegura que tus manos estén frescas y transpirables en primavera, verano y otoño. El logotipo reflectante en la parte posterior de los guantes de bicicleta añade un efecto de advertencia por la noche. Hay dos bucles en los dedos para ayudarte a quitar fácilmente los guantes de bicicleta de montaña.",
+        "Almohadillas SBR Shock": "los guantes de moto de suciedad adoptan un grueso SBR, que absorbe los golpes y es transpirable y puede darte suficiente agarre para agarrar cualquier cosa. El acolchado mantiene la palma flexible, antideslizante y a prueba de golpes, reduce la fatiga de la conducción de larga distancia. Los guantes de bicicleta son la elección perfecta para ofrecerte suficiente protección en caídas."
+      }
+    ]
+
+  },
+
+  {
+    "nombre": "NICEWIN Guantes de motocicleta para hombre",
+    "descripcion": "pantalla táctil para bicicleta de montaña y suciedad, guantes de dedo completo, carreras, ciclismo, escalada, motocross",
+    "marca": "NICEWIN",
+    "precio": 431.00,
+    "url": "assets/images/Guantes/g005_001.PNG",
+    "talla": "",
+    "especificaciones": [
+      {
+        "Protección avanzada": "el material duro cubre completamente los nudillos, mientras que las palmas están protegidas por almohadillas acolchadas. En comparación con otros productos del mercado, la protección de la palma es un 30% mayor.Las costuras de alta calidad son más apretadas que otras marcas y no se rompen ni se deshacen fácilmente. Con el uso regular, durarán más de un año.",
+        "Adhesivo de muñeca": "cómodo sin igual para un mejor ajuste. Tus manos se mantendrán frescas y sin sudor cuando se ponga duro. Esto garantiza que no evitarás la protección esencial de las manos durante actividades extremas bajo el sol ardiente. Se llevarán a cabo en cualquier deporte donde las caídas y choques sean comunes. ¡Ponte y mantente seguro!",
+        "Pantalla táctil altamente sensible": "tanto el pulgar como el dedo índice se pueden utilizar con una pantalla táctil, por lo que puedes operar fácilmente tu teléfono sin quitarte los guantes. Por favor, póngase en contacto con nosotros, si los guantes de motocicleta están defectuosos, le daremos una solución mejor que el servicio de atención al cliente de Amazon."
+      }
+    ]
+
+  },
+
+  {
+    "nombre": "NICEWIN Guantes de ciclismo de dedos completos",
+    "descripcion": "tela transpirable resistente al desgaste, almohadilla de gel absorbente de golpes, apto para montar en bicicleta de montaña en otoño e invierno",
+    "marca": "NICEWIN",
+    "precio": 347.15,
+    "url": "assets/images/Guantes/g006_001.PNG",
+    "talla": "",
+    "especificaciones": [
+      {
+        "Ajuste cómodo": "los guantes de bicicleta NICEWIN están hechos de gamuza elástica y elastano transpirable en la parte posterior, cómodos de llevar, asegurando que tus manos frescas y frescas en primavera, verano y otoño. Los guantes de conducción con forro polar en el pulgar te ayudan a limpiar fácilmente el sudor. Muñeca alargada con cierre de gancho y bucle, elegante y fácil de poner, también mantiene los guantes de ciclismo calientes durante mucho tiempo.",
+        "SBR PAD": "El acolchado SBR del guante de bicicleta de montaña es absorbente de golpes y transpirable, lo que te da suficiente agarre para agarrar cualquier cosa, cambiar de marcha. El acolchado es cómodo en tu mano, perfecto para viajes largos, que es la elección perfecta para ofrecerte suficiente protección en caídas. Los guantes de bicicleta de montar también mantienen las manos bastante calientes, en el frío proporciona suficiente calor para mantener los dedos calientes. Te proporcionarán algunos de sus fantásticos para el ciclismo.",
+        "Diseño de seguridad": "las tiras reflectantes de precaución en el dorso de la mano aumentan el efecto de advertencia en la noche. Las almohadillas especiales para la palma pueden absorber eficazmente el impacto y reducir el entumecimiento en la carretera con baches. Alivia la fatiga de la palma y reduce la probabilidad de saltar.",
+        "Pantalla táctil": "los guantes de bicicleta de carretera adoptan microfibra para que puedas acceder a teléfonos inteligentes sin quitarte los guantes de carreras para responder llamadas o responder a mensajes, lo que es realmente algo indeseable en invierno."
+
+      }
+    ]
+
+  },
+
+  {
+    "nombre": "Guantes de ciclismo para invierno",
+    "descripcion": "3 tamaños, 3 colores, almohadillas de dedos completos, transpirables, ligeros, multiusos (para ciclismo, motociclismo de montaña o libre, BMX, levantamiento de pesas, fitness, escalada)",
+    "marca": "Arltb",
+    "precio": 398.99,
+    "url": "assets/images/Guantes/g007_001.PNG",
+    "talla": "",
+    "especificaciones": [
+      {
+        "Almohadilla de palma para reducir la fricción": "los guantes de ciclismo para hombre están adoptados con un diseño de almohadilla de espuma en la palma para reducir la fricción entre la palma y el manubrio de la bicicleta. Llevar estos guantes de ciclismo con almohadilla reducirá el dolor de las palmas después de manejar de forma prolongada. Los guantes elásticos para bicicleta de montaña se ajustan perfectamente a las manos y los dedos de los ciclistas.",
+        "Material transpirable para un manejo fresco": "los guantes de ciclismo de dedos completos son suaves y ligeros, lo que no causará ninguna presión en las manos. Como se utiliza material de microfibra transpirable de calidad que absorbe rápidamente el sudor producido durante un manejo prolongado, los guantes de ciclismo de dedos completos siempre se mantienen secos y dan a los hombres y mujeres una experiencia de manejo fresca.",
+        "Diseño de paño de rizo": "los pulgares de los guantes de ciclismo de dedos completos están especialmente diseñados con tela de rizo que puede ayudar a los ciclistas a limpiar el sudor durante el manejo prolongado. Los ciclistas con los guantes de ciclismo de dedos completos no necesitan llevar toallas adicionales. Los guantes de ciclismo de color rojo también funcionan como guantes de moto de invierno para mantener a los hombres y mujeres calientes en climas fríos."
+      }
+    ]
+
+  },
+
+  {
+    "nombre": "Cool Change Guantes de bicicleta unisex",
+    "descripcion": "para exteriores con pantalla táctil",
+    "marca": "Cool Change",
+    "precio": 398.99,
+    "url": "assets/images/Guantes/g008_001.PNG",
+    "talla": "",
+    "especificaciones": [
+      {
+        "Tamaño": "pequeño",
+        "transpirable": "los guantes de ciclismo están hechos de malla transpirable que absorbe la humedad y absorbe la humedad en 4 direcciones, cómodos de llevar, que puede mantener tu mano cómoda incluso en días calurosos y se ajustan cómodamente sin estar apretada.",
+        "Almohadilla SBR y paño de rizo": "los guantes de ciclismo especializados hechos de microfibra en la palma son antideslizantes y resistentes al desgaste. El acolchado SBR absorbe los golpes y es transpirable. El diseño de tela de rizo en los pulgares se utiliza principalmente para limpiar el sudor que gotea y distrae.",
+        "Pantalla táctil": "utiliza estos guantes de ciclismo hace que sea fácil de usar al tacto tu teléfono inteligente y todo tipo de productos electrónicos de control táctil.",
+        "Multifuncional": "ideal para hombres o mujeres en primavera, verano y otoño. equitación, correr, esquí, patineta, conducción, motocross, pesca, senderismo, escalada, camping y muchas otras actividades deportivas al aire libre para garantizar la seguridad de la palma, al mismo tiempo proteger tus dedos de daños.",
+        "Nota": "hemos actualizado nuestra tabla de tallas. Es adecuado para la mayoría de los clientes, el tamaño se mide por circunferencia de la palma, si estás entre tamaños o tienes una palma más gruesa, elige una talla más grande."
+      }
+    ]
+
+  },
+
+  {
+    "nombre": "Neugood Guantes Deportivos para Pesas",
+    "descripcion": "Protegen las manos durante tus entrenamientos",
+    "marca": "Neugood",
+    "precio": 299.00,
+    "url": "assets/images/Guantes/g009_001.PNG",
+    "talla": "",
+    "especificaciones": [
+      {
+        "Microfibra": "transpirable en los costados para proporcionar frescura y gel antideslizante"
+      }
+    ]
+
+  },
+
+  {
+    "nombre": "Hikenture Guantes de ciclismo de medio dedo para hombres y mujeres",
+    "descripcion": "acolchados para bicicleta de carretera, a prueba de golpes, transpirables, para correr, motocicleta, entrenamiento",
+    "marca": "Hikenture",
+    "precio": 339.00,
+    "url": "assets/images/Guantes/g010_001.PNG",
+    "talla": "",
+    "especificaciones": [
+      {
+        "Usos": "ciclismo, senderismo, correr, conducir, equitación, ejercicio de spinning acuático y patinaje."
+      }
+    ]
+
+  }
+
+];

6c218d9b3d2cdb4960da8ef3de5e224659e987f5
Santiago-mnd <santi_ga1020@hotmail.com>
2021-07-02 11:44:52 -0500

    Agregué objetos

---
+++ gafas.js
@@ -0,0 +1,85 @@
+Cascos = [
+       
+    {
+        "nombre": "Gafas de cilismo",
+        "descripcion": "Gafas de sol polarizadas, deportivas",
+        "marca": "TECHVIDA",
+        "precio": 339.15,
+        "url": "/assets/images/Gafas/L1.jpg",
+        
+  
+    },    
+    
+    {
+        "nombre": "Gafas de cilismo UV",
+        "descripcion": "Gafas polarizadas-deportivas que protege de los aryos UV",
+        "marca": "Queshark",
+        "precio": 549.00,
+        "url": "/assets/images/Gafas/L2.jpg",
+        
+  
+    },
+
+    {
+        "nombre": "Gafas de cilismo",
+        "descripcion": "Gafas de sol polarizadas cuenta con lentes intercambiables para diferentes situaciones.",
+        "marca": "Epeak Brands",
+        "precio": 699.00,
+        "url": "/assets/images/Gafas/L3.jpg",
+        
+  
+    },
+
+    {
+        "nombre": "Gafas deportivas económicas",
+        "descripcion": "Gafas deportivas efectivas para montañismo",
+        "marca": "XIAOLW",
+        "precio": 139.29,
+        "url": "/assets/images/Gafas/L4.jpg",
+        
+  
+    },
+
+    {
+        "nombre": "Gafas económicas",
+        "descripcion": "Gafas poralizadas para ciclismo urbano.",
+        "marca": "Sunix Gafas",
+        "precio": 150.00,
+        "url": "/assets/images/Gafas/L5.jpg",
+        
+  
+    },
+
+    {
+        "nombre": "Gafas deportivas económicas",
+        "descripcion": "Gafas deportivas efectivas para montañismo",
+        "marca": "TECHVIDA",
+        "precio": 634.81,
+        "url": "/assets/images/Gafas/L6.jpg",
+        
+  
+    },
+
+    {
+        "nombre": "Gafas polarizadas",
+        "descripcion": "Gafas de montaña ",
+        "marca": "Beonsafe",
+        "precio": 360.39,
+        "url": "/assets/images/Gafas/L7.jpg",
+        
+  
+    },
+
+    {
+        "nombre": "Gafas polarizadas",
+        "descripcion": "Gafas polarizadas recomendada para todos los ámbitos.",
+        "marca": "ETCBUYS",
+        "precio": 449.99,
+        "url": "/assets/images/Gafas/L8.jpg",
+        
+  
+    },
+
+
+
+    ];

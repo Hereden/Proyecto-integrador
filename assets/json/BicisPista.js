545250dfe6782f1a5e8680bea0c8f9fdac63b141
Tamara Morcans <morcans@gmail.com>
2021-07-06 15:07:30 -0500

    Json a BicisPista

---
+++ assets/json/BicisPista.js
@@ -0,0 +1,306 @@
+BicisPista = [
+    {
+        "nombre": "Ridley Noah Fast Disc Ultegra",
+        "descripcion": "La Noah Disc es el modelo Insignia de Ridley, está lista para llevar las bicicletas aero a un nuevo nivel y no tiene rival a la hora de cortar el viento gracias a la tecnología F-Surface Plus y F-Wings",
+        "marca": "Ridley",
+        "precio": 159900,
+        "url": "assets/images/BicisPista/BP1.jpg",
+        "talla": "",
+        "especificaciones": [
+            {
+                "Cuadro": "Noah Fast Disc, 60T-40T-30T High Modulus Unidirectional Carbon, In-Mould F-Surface Technology, TA 12x142mm",
+                "Tijera": "Noah Fast Disc, 60T-40T-30T High Modulus Unidirectional Carbon, In-Mould F-Surface Technology, TA 12x100mm",
+                "Rines": "Forza R45_17CC, DT Swiss 350",
+                "Llantas": "Vittoria Corsa 25mm",
+                "Multipliación": "Rotor FLOW 52/36",
+                "Cassette": "Shimano Ultegra 11-30",
+                "Frenos": "Shimano Ultegra Flatmount",
+                "Manubrio": "Ridley Fast Integrated Cockpit",
+                "Poste Manubrio": "Ridley Fast Integrated Cockpit",
+                "Asiento": "Selle Italia SLR"
+
+
+            }
+        ]
+
+    },
+
+
+    {
+
+        "nombre": "Cannondale Caad 13 Ultegra Candy",
+        "descripcion": "Una bici de Ruta ligera, suave y rápido, el CAAD13 supera a los cuadros de carbono de muchos competidores, a un precio de aluminio. Con una excelente calidad de marcha, un manejo excepcional y una amplia versatilidad, CAAD13 cumple.",
+        "marca": "Cannondale",
+        "precio": 62499,
+        "url": "assets/images/BicisPista/BP2.jpg",
+        "talla": "51,54",
+        "especificaciones": [
+            {
+
+                "Cuadro": "SmartForm C1 Premium Alloy, 12x142 Speed Release thru-axle, flat-mount disc, integrated cable routing w/ Switchplate, SAVE, BB30a, Di2 ready",
+                "Tijera": "BallisTec Carbon, SAVE, 1-1/8 to 1-1/4 steerer, integrated crown race, 12x100mm Speed Release thru-axle",
+                "Rines": "Cannondale RD 2.0, double wall w/ eyelet, 28h",
+                "Llantas": "Vittoria Zaffiro Pro Slick, 700 x 25c",
+                "Transmision": "Shimano Ultegra, 11-speed",
+                "Multipliación": "Cannondale 1, BB30a, FSA rings, 52/36",
+                "Cassette2": "Shimano 105, 11-30, 11-speed",
+                "Frenos": "Shimano Ultegra direct mount",
+                "Manubrio": "Cannondale 3, 6061 alloy, Compact.",
+                "Poste Manubrio": "Cannondale 3, 6061 Alloy, 31.8, 7°",
+                "Asiento": "Prologo Nago RS STN"
+
+            }
+        ]
+    },
+
+
+
+    {
+
+        "nombre": "Ridley Fenix Disc Ultegra Di2",
+        "descripcion": "Ridley Fenix es la bicicleta “All Around” por excelencia, ahora en México. Ofrece la combinación perfecta entre rendimiento, peso, fortaleza, rigidez y, especialmente, confort para pasar largos días sobre el sillín",
+        "marca": "Ridley",
+        "precio": 137990,
+        "url": "assets/images/BicisPista/BP3.jpg",
+        "talla": "48,51,54,57,60",
+        "especificaciones": [
+            {
+
+
+                "Cuadro": "Fenix, 30T-24T HM UD Carbon, Diamond Shape, TA 12x142mm",
+                "Tijera": "Fenix, 30T-24T HM UD Carbon, Straight Blade, F-Steerer, TA 12x100mm",
+                "Rines": "DT Swiss ER1600 Spline DB",
+                "Llantas": "Vittoria Rubino Pro, 700 x 25c",
+                "Transmision": "Shimano Ultegra HDB Di2 11ps",
+                "Multipliación": "Rotor Vegast 50/34T",
+                "Cassette": "Shimano Ultegra, 11-speed",
+                "Frenos": "Shimano Ultegra Flatmount",
+                "Manubrio": "Forza N1 Integrated Handlebar",
+                "Poste Manubrio": "Forza N1 Integrated Handlebar",
+                "Asiento": "Forza Cirrus Pro",
+
+            }
+        ]
+    },
+
+
+    {
+
+        "nombre": "Cannondale Synapse Meteor Gray",
+        "descripcion": "La bicicleta Synapse Disc es ligera, rueda suave y te permite una posición deportiva pero cómoda para que tus salidas sean más largas, rápidas y frecuentes, es la opción más versátil para salir a rodar e incluso para ir a trabajar",
+        "marca": "Cannodale",
+        "precio": 36990,
+        "url": "assets/images/BicisPista/BP4.jpg",
+        "talla": "48,51,54,56,58",
+        "especificaciones": [
+            {
+
+                "Cuadro": "SmartForm C2 Alloy, SAVE, BB30, internal cable routing, flat mount, 130x9mm QR",
+
+                "Tijera": "BallisTec Full Carbon, integrated crown race, SAVE, 12x100mm thru-axle, internal routing",
+
+                "Rines": "RD 3.0, 28h",
+                "Llantas": "Vittoria Zaffiro, 700 x 30c",
+                "Transmision": "Shimano Tiagra, 10-speed",
+                "Multipliación": "FSA Omega ME Alloy, 50/34",
+                "Cassette": "Shimano Tiagra, 11-34, 10-speed",
+                "Frenos": "Promax Decoder R cable-actuated disc, 160/160mm",
+                "Manubrio": "Cannondale 4, 6061 alloy, Compact",
+                "Poste Manubrio": "Cannondale 4, 6061 Alloy, 31.8, 7°",
+                "Asiento": "Cannondale Stage Ergo",
+
+
+            }
+        ]
+    },
+
+
+    {
+
+        "nombre": "Cannondale nueva CAAD Optimo 3 Highlighter",
+        "descripcion": "La nueva CAAD Optimo con Shimano Sora es tú puerta de entrada al mundo de bicicletas de ruta cómodas, rápidas y ágiles. El manejo excepcional y la excelente eficiencia brindan una experiencia de carretera pura y moderna.",
+        "marca": "Cannodale",
+        "precio": 27990,
+        "url": "assets/images/BicisPista/BP5.jpg",
+        "talla": "48,51,54,56",
+        "especificaciones": [
+            {
+
+
+                "Cuadro": "All-New SmartForm C2 Alloy, SAVE, tapered headtube, fender mounts, 130x9mm QR",
+                "Tijera": "BallisTec Full Carbon, 1-1/8 to 1-1/4 steerer, fender mounts, 100x9mm QR",
+                "Rines": "RS 3.0, 24h front, 28h rear",
+                "Llantas": "Vittoria Zaffiro, 700 x 25c",
+                "Transmision": "Shimano Sora, 9-speed",
+                "Multipliación": "FSA Vero Alloy, 50/34",
+                "Cassette": "Shimano HG50, 11-30, 9-speed",
+                "Frenos": "Promax RC-452, dual pivot calipers",
+                "Manubrio": "6061 alloy, Compact",
+                "Poste Manubrio": "6061 alloy, Compact",
+                "Asiento": "Cannondale Stage CX",
+
+            }
+        ]
+    },
+
+
+    {
+
+        "nombre": "Cannondale System Six Carbon Ultegra",
+        "descripcion": "La bici de ruta con menos resistencia al viento, igual a: más velocidad, asi es la nueva System Six. Supera a sus rivales, y no solamente donde tu lo esperarías. Esprinta, desciende e incluso sube los puertos más rápido, ahorrando vatios en todo momento.",
+        "marca": "Cannodale",
+        "precio": 123900,
+        "url": "assets/images/BicisPista/BP6.jpg",
+        "talla": "51,54,56",
+        "especificaciones": [
+            {
+
+                "Cuadro": "BallisTec Carbon, integrated cable routing w/ Switchplate, SAVE, PF30a, flat mount disc, 12x142 Speed Release thru-axle",
+                "Tijera": "BallisTec Carbon, 1-1/8” - 1-1/4” steerer, 12x100mm Speed Release thru-axle, flat mount disc",
+                "Rines": "Vision SC55, carbon clincher disc, tubeless ready",
+                "Llantas": "Vittoria Rubino Pro Bright Black, 700 x 25c, reflective strip",
+                "Transmision": "Shimano Ultegra, 11-speed",
+                "Multipliación": "HollowGram, BB30a, OPI SpideRing, 52/36",
+                "Cassette": "Shimano HG601, 11-speed",
+                "Frenos": "Shimano Ultegra hydraulic disc, 160/140mm RT800 rotors",
+                "Manubrio": "HollowGram KNØT SystemBar, Carbon, 8 deg. pitch adjust",
+                "Poste Manubrio": "HollowGram KNØT, Alloy w/ cable cover, -17°",
+                "Asiento": "Prologo Dimension STN NDR, steel rails",
+
+
+
+            }
+        ]
+    },
+
+
+
+    {
+
+        "nombre": "Cannondale nueva CAAD Optimo 2 Green",
+        "descripcion": "La nueva CAAD Optimo con Shimano Tiagra es tú puerta de entrada al mundo de bicicletas de ruta cómodas, rápidas y ágiles. El manejo excepcional y la excelente eficiencia brindan una experiencia de carretera pura y moderna",
+        "marca": "Cannodale",
+        "precio": 30990,
+        "url": "assets/images/BicisPista/BP7.jpg",
+        "talla": "48,51,54,58",
+        "especificaciones": [
+            {
+
+                "Cuadro": "All-New SmartForm C2 Alloy, SAVE, tapered headtube, fender mounts, 130x9mm QR",
+                "Tijera": "BallisTec Full Carbon, 1-1/8 to 1-1/4 steerer, fender mounts, 100x9mm QR",
+                "Rines": "RS 3.0, 24h front, 28h rear",
+                "Llantas": "Vittoria Zaffiro, 700 x 25c",
+                "Transmision": "Shimano Tiagra, 2x10",
+                "Multipliación": "FSA Omega ME Alloy, 50/34",
+                "Cassette": "Shimano HG500, 11-32, 10-speed",
+                "Frenos": "Promax RC-476, dual pivot calipers",
+                "Manubrio": "6061 alloy, Compact",
+                "Poste Manubrio": "6061 Alloy, 31.8, 7°",
+                "Asiento": "Cannondale Stage CX",
+
+
+            }
+        ]
+    },
+
+
+
+    {
+
+        "nombre": "Look 785 Huez Disc Ultegra",
+        "descripcion": "Look 785 HUEZ con frenos de Disco. Una bicicleta pensada y diseñada para una mayor ligereza y eficiencia en montaña. El punto ideal entre precio y desempeño",
+        "marca": "Look",
+        "precio": 92324,
+        "url": "assets/images/BicisPista/BP8.jpg",
+        "talla": "48,54,58,",
+        "especificaciones": [
+            {
+
+                "Cuadro": "Look 785 Disc Carbon 990g 50% High Modulus y 50% High Resistance",
+                "Tijera": "Look carbon Monoblock Super Light 350g",
+                "Rines": "MAVIC Aksium Elite Disc UST",
+                "Llantas": "Mavic Yksion Pro UST",
+                "Transmision": "Shimano Ultegra R8000",
+                "Multipliación": "Shimano Ultegra R8000",
+                "Cassette": "Shimano Ultegra 11-28",
+                "Frenos": "Shimano Ultegra R8000 Hydro Disc",
+                "Manubrio": "Deda Zero 100 Alloy",
+                "Poste Manubrio": "Deda Zero 100 Alloy 90/100/110 mm",
+                "Asiento": "Selle Italia",
+
+
+
+
+            }
+        ]
+    },
+
+
+
+    {
+
+        "nombre": "Eddy Merckx Lavaredo 68 Disc Ultegra",
+        "descripcion": "La Lavaredo68 es una bicicleta de carreras de carbono que combina un gran confort con un alto nivel de rigidez. Esta versátil máquina de carreras es la combinación ideal de alto rendimiento y resistencia",
+        "marca": "Eddy Merckx",
+        "precio": 87800,
+        "url": "assets/images/BicisPista/BP9.jpg",
+        "talla": "48,51,54",
+        "especificaciones": [
+            {
+
+                "Cuadro": "LV68 Unidirectional HM Carbon, 12x142 TA",
+                "Tijera": "LV68 Unidirectional HM Carbon, 12x142 TA",
+                "Transmision": "Shimano Ultegra Mix DB",
+                "Multiplicacion": "FSA Gossamer Pro 50-34",
+                "Rines": "Fulcrum Racing 700 DB",
+
+
+            }
+        ]
+    },
+
+
+    {
+
+        "nombre": "Look 785 Huez Force Axs",
+        "descripcion": "Look 785 HUEZ con SRAM AXS. Una bicicleta pensada y diseñada para una mayor ligereza y eficiencia en montaña. Se complementa con lo último en tecnología de SRAM con 12 velocidades inalámbrico",
+        "marca": "Look",
+        "precio": 131745,
+        "url": "assets/images/BicisPista/BP10.jpg",
+        "talla": "48,54,58",
+        "especificaciones": [
+            {
+
+                "Cuadro": "Look 785 Carbon 990g 50% High Modulus y 50% High Resistance",
+                "Tijera": "Look carbon Monoblock Super Light 350g",
+                "Rines": "MAVIC Aksium Elite UST",
+                "Llantas": "Mavic Yksion Pro UST",
+                "Transmision": "SRAM Force AXS 12 vel inalámbrico",
+                "Multipliación": "Sram Force AXS new X-range 46/33, 48/35",
+                "Cassette": "SRAM XG-1270 10-28",
+                "Frenos": "SRAM Froce HRD Brake System",
+                "Manubrio": "Deda Zero 100 Alloy",
+                "Poste Manubrio": "Deda Zero 100 Alloy 90/100/110 mm",
+                "Asiento": "Selle Italia",
+
+
+
+            }
+        ]
+    }
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

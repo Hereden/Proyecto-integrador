b30348e25fa5c931bcd35b3c208592594e8bfb7d
Tamara-Morcans <tamara.morcans@comunidad.unam.mx>
2021-07-27 14:45:40 -0500

    Diagrama y SQL

--- BD/tlacuariders.sql
+++ BD/tlacuariders.sql
@@ -1,5 +1,6 @@
-create database tlacuaRiders;
-use tlacuaRidersprueba;
+#drop database tlacuaRiders;
+#create database tlacuaRiders;
+use tlacuaRiders;
 
 create table codigo_postal(
 id int not null,
@@ -10,55 +11,55 @@ constraint pk_codigo_postal primary key(id)
 );
 
 create table usuarios(
-id int not null,
+id int not null auto_increment,
 nombre varchar(255) not null,
 alias varchar(255) not null,
-correo varchar(255) not null,
+correo varchar(255) unique not null,
 contra varchar(255) not null,
 direccion varchar(255) not null, 
-telefono int(12) not null,
+telefono varchar(12) not null,
 genero int(1) not null, 
 codigo_postal int(6) not null,
 constraint pk_usuarios primary key(id)
 );
 
 create table articulos(
-id int not null,
+id int not null auto_increment,
 nombre varchar(255) not null,
 descripcion varchar(255) not null,
 talla varchar(255) not null,
 colores varchar(255) not null,
 categoria int not null,
-precio int not null,
+precio float not null,
 constraint pk_articulos primary key(id)
 );
 
 create table ventas (
-id int not null,
+id int not null auto_increment,
 usuario_id int not null,
 num_pedido int not null,
-precio_total int not null,
+precio_total float not null,
 constraint pk_ventas primary key(id)
 );
 
 create table pedidos (
-id int not null,
+id int not null auto_increment,
 num_pedido int not null,
 articulo_id int not null,
 cantidad int not null,
-precio_total int not null,
+precio_total float not null,
 constraint pk_pedido primary key(id)
 );
 
 create table rodadas(
-id int not null,
+id int not null auto_increment,
 usuario_id int not null,
 nombre varchar(255) not null,
 descripcion varchar(255) not null, 
 condicion int not null,
 direccion_inicio varchar(255) not null, 
 direccion_fin varchar(255) not null, 
-hora_encuentro time not null,
+hora_encuentro varchar(7) not null,
 bicis int not null,
 compania int not null,
 usuarios_confirmados int null,
@@ -72,3 +73,7 @@ alter table rodadas add FOREIGN key(usuario_id) references usuarios(id);
 alter table ventas add FOREIGN key(usuario_id) references usuarios(id);
 alter table pedidos add FOREIGN key(articulo_id) references articulos(id);
 alter table pedidos add FOREIGN key(num_pedido) references ventas(id);
+
+
+insert into codigo_postal values(4620,"Ciudad de México","Coyoacan", "Ciudad de México");
+insert into usuarios values(null,"Santiago","Santi","santiago@gmail.com","ilovebadbunny","iztapalapa","5546901578",1,4620)
